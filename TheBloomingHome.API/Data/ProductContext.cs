be3106595285e7e99ef5aab5b5bda076d2abc273
Egxr41k <egor2005krava@gmail.com>
2023-09-08 23:04:50 +0300

    adding ProductContext and connection string

---
+++ TheBloomingHome.API/Data/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheBloomingHome.API.Entities;
+
+namespace TheBloomingHome.API.Data;
+
+public class ProductContext : DbContext
+{
+    public ProductContext(DbContextOptions<ProductContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+}
